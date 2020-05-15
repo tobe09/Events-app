5d60cded6c2dd39fb60dd2b2698c3e9832458b34
Tobenna <tobenna@nexus23rdltd.com>
2020-05-15 16:41:45 +0100

    after removing end of file spaces from linter

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,15 +5,16 @@ import { EventsAppComponent } from './events-app.component';
 import { EventsListComponent } from './events/events-list-component';
 import { EventsThumbnailComponent } from './events/event-thumbnail-component';
 import { NavbarComponent } from './nav/navbar-component';
-import { EventService } from './events/shared/event-service';
+import { EventService } from './events/shared/event.service';
 import { ToastrService } from './common/toastr.service';
+import { EventDetailsComponent } from './events/event-details/event-details.component';
 
 @NgModule({
   imports: [
     BrowserModule
   ],
   declarations: [
-    EventsAppComponent, EventsListComponent, EventsThumbnailComponent, NavbarComponent
+    EventsAppComponent, NavbarComponent, EventsListComponent, EventsThumbnailComponent, EventDetailsComponent
   ],
   providers: [
     EventService, ToastrService
