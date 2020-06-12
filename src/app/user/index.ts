3968f4135c99e82227c13e5af622f839cae9fdca
Tobenna <tobenna@nexus23rdltd.com>
2020-06-12 10:05:54 +0100

    minimized heroku branch to hold only the server code

--- src/app/user/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './profile.component';
-export * from './login.component';
-export * from './auth.service';
--- src/app/user/user.routes.ts
+++
@@ -1,7 +0,0 @@
-import { Routes } from '@angular/router';
-import { ProfileComponent, LoginComponent } from './index';
-
-export const userRoutes: Routes = [
-    { path: 'profile', component: ProfileComponent },
-    { path: 'login', component: LoginComponent }
-];
